bd6503886d0f01bd5312a5a2fe9bf9d1379379ed
Hamza <1203302@student.birzeit.edu>
2023-01-27 17:15:17 +0200

    Ding the connection for the DB.

--- src/main/java/com/example/databaseproject/database/Database.java
+++ src/main/java/com/example/databaseproject/database/Database.java
@@ -1,20 +1,39 @@
 package com.example.databaseproject.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
     private static Database database;
-    private Database(){
-        /*
-        DO connection for the database
-         */
+    private static Connection connection;
+    private static String port = "3306";
+    private static String dbName = "parkingSystem";
+    private static String userName = "root";
+    private static String dbPassword = "root1234";
 
+    private Database() {
+        connectDB();
     }
-    static Database getInstance(){
-        if(database == null){
+
+    public static void connectDB() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:" + port + "/" + dbName + "?useLegacyDatetimeCode=true&serverTimezone=UTC&useSSL=false", userName, dbPassword);
+        } catch (SQLException e) {
+        }
+    }
+
+    static Database getInstance() {
+        if (database == null) {
             database = new Database();
         }
+
         return database;
     }
-    /*
+
+   /*
     Do an execute method for executing a String Query
      */
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.databaseproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.databaseproject to javafx.fxml;
